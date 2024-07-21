1db2af5bfb68a86cc8db88a2494632e67c80b58d
Maithili Kawale <maithilikawale16@gmail.com>
2024-07-21 19:02:57 +0530

    addresses changed

--- serverrr/index.js
+++ serverrr/index.js
@@ -99,7 +99,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: 'https://login-page-frontend-seven.vercel.app/',
+    origin: 'http://localhost:3000',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   })
