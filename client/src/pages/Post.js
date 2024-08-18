e1deaa5270c478c6046476c5692f80bd208616e6
Maithili Kawale <maithilikawale16@gmail.com>
2024-08-18 23:56:00 +0530

    post page code changed

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,34 +1,57 @@
-import React from 'react';
-import { useLocation } from 'react-router';
-import { posts } from '../data';
-import axios from 'axios'
+// import React from 'react';
+// import { useLocation } from 'react-router';
+// import { posts } from '../data';
+// import axios from 'axios'
+// const Post = () => {
+//   const location = useLocation();
+//   const path = location.pathname.split('/')[2];
+//   // axios.get('https://login-page-backend-me2p.onrender.com/auth/login/success', { withCredentials: true })
+//   // .then(response => {
+//   //   console.log(response.data);
+//   // })
+//   // .catch(error => {
+//   //   console.error(error);
+//   // });
+
+
+
+//   // Find the post with the specified ID
+//   const post = posts.find((p) => p.id.toString() === path);
+
+//   // console.log(posts)
+//   // Check if the post is undefined before accessing its properties
+//   if (!post) {
+//     return <div className="post">Post not found</div>;
+//   }
+
+//   console.log(location);
+
+//   return (
+//     <div className="post">
+//       <img src={post.img} alt="xyz" className="postImg" />
+//       <h1 className="postTitle">{post.title}</h1>
+//       <p className="postDesc">{post.desc}</p>
+//       <p className="postLongDesc">{post.longDesc}</p>
+//     </div>
+//   );
+// };
+
+// export default Post;
+
+
+import { useLocation } from "react-router";
+import { posts } from "../data";
+
 const Post = () => {
   const location = useLocation();
-  const path = location.pathname.split('/')[2];
-  axios.get('https://login-page-backend-me2p.onrender.com/auth/login/success', { withCredentials: true })
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+  const path = location.pathname.split("/")[2];
 
-
-
-  // Find the post with the specified ID
   const post = posts.find((p) => p.id.toString() === path);
 
-  console.log(posts)
-  // Check if the post is undefined before accessing its properties
-  if (!post) {
-    return <div className="post">Post not found</div>;
-  }
-
   console.log(location);
-
   return (
     <div className="post">
-      <img src={post.img} alt="xyz" className="postImg" />
+      <img src={post.img} alt="" className="postImg" />
       <h1 className="postTitle">{post.title}</h1>
       <p className="postDesc">{post.desc}</p>
       <p className="postLongDesc">{post.longDesc}</p>
